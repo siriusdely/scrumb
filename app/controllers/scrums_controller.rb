be1f38d4483951cbef231c29dfe3bfb9ba1b017b
Sirius Dely <siriusdely@icloud.com>
2018-06-01 18:44:45 +0700

    clean API controllers

--- app/controllers/scrums_controller.rb
+++ app/controllers/scrums_controller.rb
@@ -1,51 +1,13 @@
-class ScrumsController < ApplicationController
-  before_action :set_scrum, only: [:show, :update, :destroy]
-
+class ScrumsController < ApiController
   # GET /scrums
   def index
-    @scrums = Scrum.all
-
-    render json: @scrums
+    @scrums = Scrum.select("id, title").all
+    render json: @scrums.to_json
   end
 
-  # GET /scrums/1
+  # GET /scrums/:id
   def show
-    render json: @scrum
+    @scrum = Scrum.find(params[:id])
+    render json: @scrum.to_json(:include => { :items => { :only => [:id, :description] } } )
   end
-
-  # POST /scrums
-  def create
-    @scrum = Scrum.new(scrum_params)
-
-    if @scrum.save
-      render json: @scrum, status: :created, location: @scrum
-    else
-      render json: @scrum.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /scrums/1
-  def update
-    if @scrum.update(scrum_params)
-      render json: @scrum
-    else
-      render json: @scrum.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /scrums/1
-  def destroy
-    @scrum.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_scrum
-      @scrum = Scrum.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def scrum_params
-      params.require(:scrum).permit(:title, :description, :tasks, :link)
-    end
 end
