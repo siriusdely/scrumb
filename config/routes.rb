d1d7339b91532b059ba4b55916f4f42d83556b0c
Sirius Dely <siriusdely@icloud.com>
2018-06-02 19:29:57 +0700

    add fallback_index_html for serving all requests to scrumb client app

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   scope '/api' do
     resources :scrums
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
