fb2c66be07394d41c80de1850374ac1d0293662f
Sirius Dely <siriusdely@icloud.com>
2018-06-10 15:33:34 +0700

    not working :|

---
+++ scrumb/src/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: {
+      // { test: /\.css$/, loader: 'style!css' }
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    }
+  }
+}
