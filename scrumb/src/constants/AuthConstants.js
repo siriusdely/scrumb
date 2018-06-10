b8e474dda65efb1332c3c672c5996c6b9fa375cc
Sirius Dely <siriusdely@icloud.com>
2018-06-10 23:56:15 +0700

    test /auth/login api successfully

---
+++ scrumb/src/constants/AuthConstants.js
@@ -0,0 +1,7 @@
+const AuthConstants = {
+  LOGIN_URL: '/api/v1/auth/login',
+  USER_LOGIN: 'USER_LOGIN',
+  USER_LOGOUT: 'USER_LOGOUT'
+}
+
+module.exports = AuthConstants;
--- scrumb/src/services/AuthService.js
+++ scrumb/src/services/AuthService.js
@@ -1,11 +1,47 @@
-export default {
-  isAuthenticated: false,
+import { LOGIN_URL } from '../constants/AuthConstants';
+
+class AuthService {
+  isAuthenticated = false;
+
   authenticate(cb) {
+    // this.login("asdf@asdf.asdf", "asdfasdf");
     this.isAuthenticated = true;
     setTimeout(cb, 100); // fake async
-  },
+  }
+
   signout(cb) {
     this.isAuthenticated = false;
     setTimeout(cb, 100);
   }
+
+  login(email, password) {
+    return window
+      .fetch(LOGIN_URL, {
+        method: 'POST',
+        body: JSON.stringify({
+          'email': email,
+          'password': password
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.log(error));
+  }
+
+  fetch(endpoint) {
+    return window
+      .fetch(endpoint, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => response.json())
+       // .then(json => console.log(json))
+      .catch(error => console.log(error));
+  }
 }
+
+export default new AuthService();
