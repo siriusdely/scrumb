a8a1739b00d9847172245c2842afd6f8e4ba7f0b
Sirius Dely <siriusdely@icloud.com>
2018-06-13 20:29:10 +0700

    fix fallback_index including rendering without layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def fallback_index_html
-    render :file => 'public/index.html'
+    render :file => 'public/index.html', :layout => nil
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -28,8 +28,8 @@ Rails.application.routes.draw do
     end
   end
 
-  # get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-  get '*path', to: "static_pages#index", constraints: ->(request) do
+  # get '*path', to: "static_pages#index", constraints: ->(request) do
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
 end
