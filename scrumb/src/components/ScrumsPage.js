e742a32706d7c2b04a302ed960ec6c3d57a41bab
Sirius Dely <siriusdely@icloud.com>
2018-07-13 22:56:31 +0700

    add New Scrum Modal Form inside ScrumsPage

--- scrumb/src/components/ScrumsPage.js
+++ scrumb/src/components/ScrumsPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {
+  Button,
   Card,
   Container,
   Dimmer,
+  Form,
   Icon,
-  Loader
+  Loader,
+  Modal
 } from 'semantic-ui-react';
 
 import ScrumService from '../services/ScrumService';
@@ -14,24 +17,39 @@ export default class ScrumsPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      scrums: ScrumStore.scrums
+      description: '',
+      modal: false,
+      scrums: ScrumStore.scrums,
+      title: ''
     };
   }
 
-  onStoreChange() {
+  handleStoreChange() {
     this.setState({
       scrums: ScrumStore.scrums
     });
   }
 
   componentDidMount() {
-    this.onStoreChange = this.onStoreChange.bind(this);
-    ScrumStore.addChangeListener(this.onStoreChange);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    ScrumStore.addChangeListener(this.handleStoreChange);
     ScrumService.getScrums();
   }
 
   componentWillUnmount() {
-    ScrumStore.removeChangeListener(this.onStoreChange);
+    ScrumStore.removeChangeListener(this.handleStoreChange);
+  }
+
+  handleModal(open) {
+    this.setState({
+      modal: open
+    });
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
 
   render() {
@@ -39,33 +57,64 @@ export default class ScrumsPage extends React.Component {
     if (scrums) {
       return (
         <Container text>
-          {
-            scrums && scrums.length ?
-            <Card.Group centered itemsPerRow="2">
-              {
-                scrums.map((scrum) => {
-                  return (
-                    <Card key={ scrum.id }
-                          color='teal'
-                          href='#card-example-link-card'>
-                      <Card.Content>
-                        <Card.Header>{ scrum.title }</Card.Header>
-                        <Card.Meta>
-                          <span className='date'>{ scrum['updated_at'] }</span>
-                        </Card.Meta>
-                        <Card.Description>{ scrum.description }</Card.Description>
-                      </Card.Content>
-                      <Card.Content extra>
-                        <Icon name='user' />
-                        { scrum['created_at'] }
-                      </Card.Content>
-                    </Card>
-                  );
-                })
-              }
-            </Card.Group>
-            : <Container textAlign='center'>No scrums found.</Container>
-          }
+          <Modal dimmer='blurring' size='small'
+                 closeOnEscape={ true } closeOnDimmerClick={ true }
+                 open={ this.state.modal }
+                 onClose={ () => { this.handleModal(false) } }>
+            <Modal.Header>Create a New Scrum</Modal.Header>
+            <Modal.Content>
+              <Form>
+                <Form.Input label='Title:' placeholder='title..'
+                            name='title' value={ this.state.title }
+                            onChange={ this.handleChange.bind(this) } />
+                <Form.TextArea label='Description' placeholder='More information about the scrum..'
+                               name='description' value={ this.state.description }
+                               onChange={ this.handleChange.bind(this) } />
+              </Form>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button color='black' onClick={ () => this.handleModal(false) }>
+                Cancel
+              </Button>
+              <Button positive icon='checkmark' labelPosition='right'
+                content="Save" onClick={ () => this.handleModal(false) }
+              />
+            </Modal.Actions>
+          </Modal>
+          <Card.Group centered itemsPerRow="2">
+            <Card color='grey' >
+              <Card.Content textAlign='center'>
+                <a onClick={ () => { this.handleModal(true) } }>
+                  <Card.Description>
+                    <Icon name='add' circular size='huge'
+                          inverted color='grey' />
+                  </Card.Description>
+                </a>
+              </Card.Content>
+            </Card>
+            {
+              scrums &&
+              scrums.map((scrum) => {
+                return (
+                  <Card key={ scrum.id }
+                        color='teal'
+                        href='#card-example-link-card'>
+                    <Card.Content>
+                      <Card.Header>{ scrum.title }</Card.Header>
+                      <Card.Meta>
+                        <span className='date'>{ scrum['updated_at'] }</span>
+                      </Card.Meta>
+                      <Card.Description>{ scrum.description }</Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <Icon name='user' />
+                      { scrum['created_at'] }
+                    </Card.Content>
+                  </Card>
+                );
+              })
+            }
+          </Card.Group>
         </Container>
       );
     } else {
