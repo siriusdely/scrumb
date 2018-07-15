7b2f8b4d77c6593013341dd0b7c7de7208dcf421
Sirius Dely <siriusdely@icloud.com>
2018-07-15 17:17:21 +0700

    refactor for Discussions layout improvement

--- scrumb/src/components/DiscussionsList.js
+++ scrumb/src/components/DiscussionsList.js
@@ -1,81 +1,22 @@
 import React from 'react';
-import { Container
-       , Grid
-       , Header
-       , List } from 'semantic-ui-react';
+import { Menu } from 'semantic-ui-react';
 
-import NewDiscussionForm from './NewDiscussionForm';
-import MessagesSection from './MessagesSection';
 import DiscussionElement from './DiscussionElement';
 
-import ChatService from '../services/ChatService';
-import ChatStore from '../stores/ChatStore';
-
 export default class DiscussionsList extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.cable = ChatService.initCable();
-    this.state = {
-      discussions: ChatStore.discussions,
-      activeDiscussionId: ChatStore.currentDiscussion ? ChatStore.currentDiscussion.id : null,
-    };
-  }
-
-  _chatStoreChange() {
-    this.setState({
-      discussions: ChatStore.discussions,
-      activeDiscussionId: ChatStore.currentDiscussion.id,
-    });
-  }
-
-  componentDidMount = () => {
-    this.chatStoreChange = this._chatStoreChange.bind(this);
-    ChatStore.addChangeListener(this.chatStoreChange);
-    ChatService.getDiscussions();
-  };
-
-  componentWillUnmount = () => {
-    ChatService.deinitCable();
-    ChatStore.removeChangeListener(this.chatStoreChange);
-  }
-
-  handleClick = id => {
-    this.setState({ activeDiscussionId: id });
-  };
-
   render = () => {
-    const { discussions, activeDiscussionId } = this.state;
+    const { activeDiscussionId, discussions, handleClick } = this.props;
     return (
-      <Container text>
-        <Grid>
-          <Grid.Row>
-            <Header as='h2'>Discussions</Header>
-          </Grid.Row>
-          <NewDiscussionForm />
-          <Grid.Row>
-            <Grid.Column width={ 5 }>
-              <List selection verticalAlign='middle'>
-                {
-                  discussions ?
-                  discussions.map(discussion =>
-                    <DiscussionElement
-                      key={ discussion.id }
-                      discussion={ discussion }
-                      handleClick={ this.handleClick } />
-                  ) : null
-                }
-              </List>
-            </Grid.Column>
-            <Grid.Column width={ 11 } stretched>
-              { activeDiscussionId ? (
-                  <MessagesSection
-                    discussion={ ChatStore.findDiscussion(activeDiscussionId) } />
-              ) : null }
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
+      <Menu fluid vertical tabular size='large'>
+      {
+        discussions ?
+        discussions.map(discussion =>
+          <DiscussionElement key={ discussion.id }
+                             discussion={ discussion } handleClick={ handleClick }
+                             active={ activeDiscussionId === discussion.id } />
+        ) : null
+      }
+      </Menu>
     );
   };
 }
--- scrumb/src/components/DiscussionsPage.js
+++ scrumb/src/components/DiscussionsPage.js
@@ -1,10 +1,66 @@
 import React from 'react';
+import { Container, Grid, Header } from 'semantic-ui-react';
+
 import DiscussionsList from './DiscussionsList';
+import MessagesSection from './MessagesSection';
+import NewDiscussionForm from './NewDiscussionForm';
+
+import ChatService from '../services/ChatService';
+import ChatStore from '../stores/ChatStore';
 
 export default class DiscussionsPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.cable = ChatService.initCable();
+    this.state = {
+      discussions: ChatStore.discussions,
+      activeDiscussionId: ChatStore.currentDiscussion ? ChatStore.currentDiscussion.id : null,
+    };
+  }
+
+  _chatStoreChange() {
+    this.setState({
+      discussions: ChatStore.discussions,
+      activeDiscussionId: ChatStore.currentDiscussion.id,
+    });
+  }
+
+  componentDidMount = () => {
+    this.chatStoreChange = this._chatStoreChange.bind(this);
+    ChatStore.addChangeListener(this.chatStoreChange);
+    ChatService.getDiscussions();
+  };
+
+  componentWillUnmount = () => {
+    ChatService.deinitCable();
+    ChatStore.removeChangeListener(this.chatStoreChange);
+  }
+
+  handleClick = id => {
+    this.setState({ activeDiscussionId: id });
+  };
+
   render() {
+    const { discussions, activeDiscussionId } = this.state;
+
     return (
-      <DiscussionsList />
+      <Container text>
+        <Grid>
+          <Grid.Column width={ 5 }>
+            <Header as='h2'>Topics</Header>
+            <NewDiscussionForm />
+            <DiscussionsList discussions={ discussions }
+                             handleClick={ this.handleClick }
+                             activeDiscussionId={ activeDiscussionId } />
+          </Grid.Column>
+          <Grid.Column width={ 11 } stretched>
+            { activeDiscussionId ? (
+                <MessagesSection
+                  discussion={ ChatStore.findDiscussion(activeDiscussionId) } />
+            ) : null }
+          </Grid.Column>
+        </Grid>
+      </Container>
     );
   }
 }
