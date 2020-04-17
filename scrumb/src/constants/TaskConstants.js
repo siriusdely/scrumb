7b87b57b08b0fb25d350cce0e7605c4ee56573bc
Sirius Dely <siriusdely@icloud.com>
2020-04-17 22:13:48 +0700

    add TaskConstants

---
+++ scrumb/src/constants/TaskConstants.js
@@ -0,0 +1,7 @@
+export const TASKS_URL = '/api/v1/tasks';
+
+export const ADD_TASK = 'ADD_TASK';
+export const DELETE_TASK = 'DELETE_TASK';
+export const TOGGLE_TASK = 'TOGGLE_TASK';
+export const TOGGLE_TASK_FAILED = 'TOGGLE_TASK_FAILED';
+export const TOGGLE_TASK_SUCCEED = 'TOGGLE_TASK_SUCCEED';
