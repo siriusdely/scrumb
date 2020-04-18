cdced9a223f73ff3421d2e9fe3965baed7f4b096
Sirius Dely <siriusdely@icloud.com>
2020-04-18 07:02:20 +0700

    rename DailyScrumApp -> DailyScrumRoot (2)

--- scrumb/src/containers/DailyScrumRoot.js
+++ scrumb/src/containers/DailyScrumRoot.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import DailyScrumPage from '../components/DailyScrumPage';
 import dailyStore from '../stores/dailyStore';
 
-export default class DailyScrumApp extends Component {
+export default class DailyScrumRoot extends Component {
   render() {
     return (
       <Provider store={ dailyStore }>
