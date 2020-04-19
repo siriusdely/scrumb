84b5d3c63adc4a5621268df2093262ea8e899518
Sirius Dely <siriusdely@icloud.com>
2020-04-19 17:24:37 +0700

    working updating task's description

--- scrumb/src/containers/SingleTaskContainer.js
+++ scrumb/src/containers/SingleTaskContainer.js
@@ -10,6 +10,14 @@ import SingleTaskItem from '../components/SingleTaskItem';
 const mapDispatchToProps = (dispatch, ownProps) => {
   // console.log('SingleTaskContainer mapDispatchToProps ownProps', ownProps);
   return {
+    onDescriptionChange: (description) => {
+      const _task = ownProps.task;
+      const task = {
+        ..._task,
+        description,
+      };
+      return dispatch(updateTask(task));
+    },
     onToggle: () => dispatch(toggleTask(ownProps.task.id)),
     onTitleChange: (title) => {
       const _task = ownProps.task;
